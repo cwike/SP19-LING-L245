050817ef0991d74375e54490961c875fe736b11c
cwike <wike.connor@gmail.com>
2019-02-18 16:55:44 -0500

    Add files via upload

--- spellchecker.py
+++ spellchecker.py
@@ -1,1 +1,23 @@
-fl = open('wiki.hist', 'w+')
+import sys
+
+
+
+fl = open('wiki.txt', 'r')
+words = fl.read()
+freq = words.split()
+
+
+##for line in fl.readlines():
+##    line = line.strip('\n')
+##    (f, w) = line.split('\t')
+##    freq.append((int(f), w))
+
+text = "Houston Hoston is awesome"
+text1 = text.split()
+for words in text1:
+    if words in freq:
+        print (words, end=' ')
+    else:
+        print(words+'*', end=' ')
+
+
